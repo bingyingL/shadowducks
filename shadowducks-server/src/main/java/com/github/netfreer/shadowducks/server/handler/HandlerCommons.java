b32ed70ff44eba78faa735dedde3935ff3ba88be
LandyKing <landykingdom@gmail.com>
2017-04-20 23:58:38 +0800

    IP address parse use internal method.

--- shadowducks-server/src/main/java/com/github/netfreer/shadowducks/server/handler/HandlerCommons.java
+++ shadowducks-server/src/main/java/com/github/netfreer/shadowducks/server/handler/HandlerCommons.java
@@ -4,34 +4,35 @@ import com.github.netfreer.shadowducks.common.utils.AppConstans;
 import io.netty.buffer.ByteBuf;
 import io.netty.util.CharsetUtil;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author: landy
  * @date: 2017-04-16 23:03
  */
 public class HandlerCommons {
-    public static InetSocketAddress tryParseAddress(ByteBuf buf) {
+    public static InetSocketAddress tryParseAddress(ByteBuf buf) throws UnknownHostException {
         buf.markReaderIndex();
         InetSocketAddress address = null;
         if (buf.readableBytes() > 3) {
             short addressType = buf.readUnsignedByte();
             if (addressType == AppConstans.IPv4) {
                 if (buf.readableBytes() >= 6) {
-                    String ip = buf.readUnsignedByte() + "." + buf.readUnsignedByte()
-                            + "." + buf.readUnsignedByte() + "." + buf.readUnsignedByte();
+                    byte[] data = new byte[4];
+                    buf.readBytes(data);
+                    InetAddress inetAddress = InetAddress.getByAddress(data);
                     int port = buf.readUnsignedShort();
-                    address = new InetSocketAddress(ip, port);
+                    address = new InetSocketAddress(inetAddress, port);
                 }
             } else if (addressType == AppConstans.IPv6) {
-                if (buf.readableBytes() >= 16) {
-                    String host = String.format("%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x",
-                            buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte(),
-                            buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte(),
-                            buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte(),
-                            buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte());
+                if (buf.readableBytes() >= 18) {
+                    byte[] data = new byte[16];
+                    buf.readBytes(data);
+                    InetAddress inetAddress = InetAddress.getByAddress(data);
                     int port = buf.readUnsignedShort();
-                    address = new InetSocketAddress(host, port);
+                    address = new InetSocketAddress(inetAddress, port);
                 }
             } else if (addressType == AppConstans.Domain) {
                 short len = buf.readUnsignedByte();
