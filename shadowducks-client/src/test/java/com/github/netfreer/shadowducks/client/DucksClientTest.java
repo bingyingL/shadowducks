ee4e06002c0151858d8d32c792eaef530aa2ba6c
LandyKing <landykingdom@gmail.com>
2017-05-02 22:51:12 +0800

    测试用例

---
+++ shadowducks-client/src/test/java/com/github/netfreer/shadowducks/client/DucksClientTest.java
@@ -0,0 +1,23 @@
+package com.github.netfreer.shadowducks.client;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * @author: landy
+ * @date: 2017-05-02 22:39
+ */
+public class DucksClientTest {
+    @Test
+    public void test1() throws Exception {
+        // 生成临时测试文件
+        // 计算临时测试文件md5
+        // 启动测试服务
+        // 启动DucksClient
+        // 启动测试客户端
+        // 通过测试客户端下载测试文件
+        // 检查下载到的文件与源文件md5是否一致
+    }
+}
